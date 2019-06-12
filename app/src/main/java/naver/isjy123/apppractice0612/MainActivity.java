186495ac19fb725792807bb04c0829b2350fdac3
503_25 <a503_25@503-25ui-iMac.local>
2019-06-12 11:12:15 +0900

    Initial commit

---
+++ app/src/main/java/naver/isjy123/apppractice0612/MainActivity.java
@@ -0,0 +1,14 @@
+package naver.isjy123.apppractice0612;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
